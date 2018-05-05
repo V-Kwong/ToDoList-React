d71190bd8d25ef455809f9f0fcb2995cc8486ea0
Vincent Kwong <vincent_kwong578@hotmail.com>
2018-05-04 17:00:28 -0700

    persistent data

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput, View, FlatList } from 'react-native';
+import { AsyncStorage, TextInput, View, FlatList } from 'react-native';
 import TodoItem from '../components/todoItem'
 import styles from '../styles/todoStyles';
 
@@ -12,16 +12,58 @@ class Todo extends Component {
     };
   }
 
-  onSubmit = () => {
-    if (this.state.text) {
-      this.setState(prevState => ({
-        text: '',
-        todos: [...prevState.todos,
-          { title: prevState.text, done: false }]
-      }));
-      this.textInput.setNativeProps({ text: '' });
+  componentDidMount = async () => {
+    try {
+      const jsonToDo = await AsyncStorage.getItem('todo');
+      if (jsonToDo !== null) {
+        this.setState({ 'todos': JSON.parse(jsonToDo) });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
+
+  onSubmit = async () => {
+    if (this.state.text) {
+      const newToDo = [...this.state.todos,
+        { title: this.state.text, done: false }];
+      
+      this.setState({
+        text: '',
+        todos: newToDo
+      })
+
+      // this.setState(prevState => ({
+      //   text: '',
+      //   todos: [...prevState.todos,
+      //     { title: prevState.text, done: false }]
+      // }));
+      this.textInput.setNativeProps({ text: '' });
+
+      // const newToDo = [...this.state.todos,
+      //   { title: this.state.text, done: false }];
+      // console.log(newToDo);
+      // var jsonString = JSON.stringify(newToDo);
+      // console.log(jsonString);
+      // console.log(JSON.parse(jsonString));
+
+      try {
+        await AsyncStorage.setItem('todo', JSON.stringify(newToDo));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  // setName = async () => {
+  //   try {
+  //     const newName = this.state.value;
+  //     await AsyncStorage.setItem('name', newName);
+  //     this.setState({ 'saved': newName });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   keyExtractor = (item, index) => index;
 
